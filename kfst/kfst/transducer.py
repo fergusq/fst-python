c70cfc77ba4d8e1018bab5859f2a2b3b656b9aa1
Iikka Hauhio <iikka.hauhio@kielikone.fi>
2023-07-07 13:31:01 +0300

    added from __future__ import annotations to make it compatible with python 3.8

--- kfst/kfst/transducer.py
+++ kfst/kfst/transducer.py
@@ -15,6 +15,8 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with KFST. If not, see <https://www.gnu.org/licenses/>. 
 
+from __future__ import annotations
+
 import argparse
 import lzma
 import re
