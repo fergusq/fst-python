069c9df06e7623cf1442d48fe61c5d306ac9b8cb
Théo Salmenkivi-Friberg <fsmnarmosta@posteo.net>
2025-07-11 15:20:14 +0300

    Lisätty testejä + bump

--- general-tests/api_test.py
+++ general-tests/api_test.py
@@ -29,4 +29,26 @@ for k in [kfst, kfst_py]:
     fst.to_kfst_file("/tmp/test.kfst")
     fst.to_kfst_file(Path("/tmp/test.kfst"))
 
+    print("Check that lookup has sane tokenization settings")
+
+    # Should work cleanly
+
+    text = "🐈‍⬛" # <- not found in Voikko
+    fst.split_to_symbols(text)
+    fst.lookup(text)
+    assert fst.split_to_symbols(text, True) is not None
+
+    list(fst.lookup(text, k.transducer.FSTState(0), True))
+
+    # Should break
+
+    assert fst.split_to_symbols(text, False) is None
+
+    try:
+        list(fst.lookup(text, k.transducer.FSTState(0), False))
+    except k.TokenizationException:
+        pass
+    else:
+        assert False
+
     print(f"Total {time.time() - t:0.2f} seconds.")
